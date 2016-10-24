1f57f3e5f4064370181eda22ddb3f9a3694f50d4
morganric <morganric@gmail.com>
2016-10-24 20:33:03 +0100

    card

--- config/routes.rb
+++ config/routes.rb
@@ -39,8 +39,8 @@ Rails.application.routes.draw do
   get '/:username/:id', to: 'posts#show', as: :user_post
 
   authenticated :user do
-    root to: 'posts#index'
-    # root to: 'pages#welcome'
+    #root to: 'posts#index'
+    root to: 'visitors#index'
   end
 
   unauthenticated do
